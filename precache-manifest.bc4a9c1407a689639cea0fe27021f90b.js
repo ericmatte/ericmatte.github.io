a995b04e4b2408d9f798b2463fecfe548b99acc1
Eric Matte <14026234@usherbrooke.ca>
2019-01-12 20:15:40 -0500

    Updates

---
+++ precache-manifest.bc4a9c1407a689639cea0fe27021f90b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6478863b0215d6031c6e283a78a6a534",
+    "url": "/static/media/top.6478863b.jpg"
+  },
+  {
+    "revision": "090717dcd3b65551a407f2809cd4efbf",
+    "url": "/static/media/separator.090717dc.jpg"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10790d492d4161a803d9",
+    "url": "/static/js/main.10790d49.chunk.js"
+  },
+  {
+    "revision": "4a71325b14748d90bff6",
+    "url": "/static/js/1.4a71325b.chunk.js"
+  },
+  {
+    "revision": "10790d492d4161a803d9",
+    "url": "/static/css/main.a6df3e71.chunk.css"
+  },
+  {
+    "revision": "4a71325b14748d90bff6",
+    "url": "/static/css/1.cc1fc997.chunk.css"
+  },
+  {
+    "revision": "9fa236c70ff7da551d988ee2d7d5e87c",
+    "url": "/index.html"
+  }
+];
