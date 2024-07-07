f4a7bb3b25a21eac52a4071b01ff1e8e2123e27a
Eric Matte <eric_matte@outlook.com>
2024-07-07 10:52:56 -0400

    Cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,6 @@ const config: Config = {
         primary: `rgba(var(--primary), <alpha-value>)`,
         secondary: `rgba(var(--secondary), <alpha-value>)`,
       },
-      fontSize: {
-        "2xs": ["0.625rem", { lineHeight: "0.8rem" }],
-        "4.5xl": ["2.5rem", { lineHeight: "3rem" }],
-      },
-      backgroundImage: {
-        "gradient-picture": "linear-gradient(24deg, var(--tw-gradient-stops))",
-      },
     },
 
     fontFamily: {
